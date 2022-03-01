cbfdd277668bbeacc361bcef99e714eb13647db3
wushileilei <wushileilei@gmail.com>
2022-03-01 10:50:53 -0500

    0301

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import FirstComponent from './components/learning-examples/FirstComponent'
+//import SecondComponent from './components/learning-examples/SecondComponent'
+//import ThirdComponent from './components/learning-examples/ThirdComponent'
+//import Counter from './components/counter/Counter'
+import TodoApp from './components/todo/TodoApp'
 import './App.css';
-import Counter from './components/Counter';
+import './bootstrap.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter></Counter>
+        {/*<Counter/>*/}
+        <TodoApp />
       </div>
     );
   }
 }
-function AnotherComponent() {
-  return(
-  <div className="anotherComponent">
-    Another Component
-  </div> 
-  );
-}
 
+// class LearningComponents extends Component {
+//   render() {
+//     return (
+//       <div className="LearningComponents">
+//          My Hello World
+//          <FirstComponent></FirstComponent>
+//          <SecondComponent></SecondComponent>
+//          <ThirdComponent></ThirdComponent>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
